997b8d32031e9afc0328c5c8fce1b0033f39b77c
John Webb <John.Webb@infusionsoft.com>
2020-08-19 21:21:08 -0700

    Minimizing size to 71 B

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,32 @@
 #! /usr/bin/env bash
 
-pushd pkg
-wasm-pack build
-npm link
-popd
 
-pushd site
-npm link rust-web
-npm i
-popd
+
+function build {
+  if [ -f 'site/opt.wasm' ]; then
+    echo 'Cleaning wasm file'
+    rm site/opt.wasm
+  fi
+
+  cargo build --target wasm32-unknown-unknown --release
+  wasm-strip "target/wasm32-unknown-unknown/release/$1"
+  wasm-opt -o "site/$2" -Oz "target/wasm32-unknown-unknown/release/$1"
+}
+
+function size_check {
+  size=`ls -lh site | awk -F ' ' '{sum+=$5;} END{print sum}'`
+
+  if [[ $size -gt 13000 ]]; then
+    echo "Over 13 size limit"
+  fi
+
+  echo "Size is: $size B"
+}
+
+function main {
+  build "rust_web.wasm" "opt.wasm"
+
+  size_check
+}
+
+main
--- bin/dev.sh
+++ bin/dev.sh
@@ -1,7 +1,3 @@
 #! /usr/bin/env bash
 
 ./bin/build.sh
-
-pushd site
-npm run serve
-popd site;
