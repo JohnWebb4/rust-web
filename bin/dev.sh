4587589b7e5821e42fcac6c1b53a6bbe4aed9b8b
John Webb <john.webb94@gmail.com>
2026-03-28 19:19:33 -0700

    Updating package versions

--- bin/dev.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-./bin/build.sh || { echo 'Build failed'; exit 1; }
-
-npx nodemon server.js
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use core::sync::atomic::{AtomicU32, Ordering};
+use wasm_bindgen::prelude::*;
 
 const HEIGHT: usize = 350;
 const SPEED: u32 = 1;
@@ -7,26 +8,24 @@ const WIDTH: usize = 900;
 
 static FRAME: AtomicU32 = AtomicU32::new(START_FRAME);
 
-extern "C" {
-    fn console_log(i: u8);
-}
-
-#[no_mangle]
 static mut BUFFER: [u8; 4 * WIDTH * HEIGHT] = [0; 4 * WIDTH * HEIGHT];
 
-#[no_mangle]
+#[wasm_bindgen]
 pub unsafe extern "C" fn run() {
-    render_frame_safe(&mut BUFFER);
+    render_frame_safe(&raw mut BUFFER);
 }
 
-fn render_frame_safe(buffer: &mut [u8; 4 * WIDTH * HEIGHT]) {
+fn render_frame_safe(buffer: *mut [u8; 4 * WIDTH * HEIGHT]) {
     let f = FRAME.fetch_add(SPEED, Ordering::Relaxed);
 
     for y in 0..HEIGHT {
         for x in 0..4 * WIDTH {
             let pixel: u32 = ((x * y) | 0xFF_00_FF_00) as u32;
 
-            buffer[4 * y * WIDTH + x] = pixel.wrapping_add(f) as u8;
+            unsafe {
+                let target = buffer.offset((4 * y * WIDTH + x) as isize);
+                *target = pixel.wrapping_add(f) as u8;
+            }
         }
     }
 }
