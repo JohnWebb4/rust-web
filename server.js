d92de97f8e43f5e2057e7c6b1ae43ef1b9c33425
John Webb <John.Webb94@gmail.com>
2026-03-28 20:52:05 -0700

    RW-1: Upgrade package versions (#1)
    * Updating package versions
    
    * RW-1: Upgrade rust version

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = 8080;
 
-app.use(express.static("site"));
+app.use(express.static("./"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+use core::cell::UnsafeCell;
 use core::sync::atomic::{AtomicU32, Ordering};
+use wasm_bindgen::prelude::*;
 
 const HEIGHT: usize = 350;
 const SPEED: u32 = 1;
@@ -7,25 +9,24 @@ const WIDTH: usize = 900;
 
 static FRAME: AtomicU32 = AtomicU32::new(START_FRAME);
 
-extern "C" {
-    fn console_log(i: u8);
+struct SyncBuffer(UnsafeCell<[u8; 4 * WIDTH * HEIGHT]>);
+unsafe impl Sync for SyncBuffer {}
+
+static BUFFER: SyncBuffer = SyncBuffer(UnsafeCell::new([0; 4 * WIDTH * HEIGHT]));
+
+#[wasm_bindgen]
+pub fn buffer_ptr() -> *mut u8 {
+    BUFFER.0.get() as *mut u8
 }
 
-#[no_mangle]
-static mut BUFFER: [u8; 4 * WIDTH * HEIGHT] = [0; 4 * WIDTH * HEIGHT];
-
-#[no_mangle]
-pub unsafe extern "C" fn run() {
-    render_frame_safe(&mut BUFFER);
-}
-
-fn render_frame_safe(buffer: &mut [u8; 4 * WIDTH * HEIGHT]) {
+#[wasm_bindgen]
+pub fn run() {
     let f = FRAME.fetch_add(SPEED, Ordering::Relaxed);
+    let buffer = unsafe { &mut *BUFFER.0.get() };
 
     for y in 0..HEIGHT {
         for x in 0..4 * WIDTH {
             let pixel: u32 = ((x * y) | 0xFF_00_FF_00) as u32;
-
             buffer[4 * y * WIDTH + x] = pixel.wrapping_add(f) as u8;
         }
     }
